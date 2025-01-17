f505df1c3002e1f04f011da9d03e70e4326baf23
Filipe do Amaral Padilha <filipe.padilha@maxima.local>
2025-01-17 03:57:51 -0300

    Incluindo as dependências Angular e adicionando CORS

--- DotNetApiMaxima/Program.cs
+++ DotNetApiMaxima/Program.cs
@@ -12,12 +12,19 @@ builder.Services.AddControllers(); // Adiciona suporte a Controllers
 builder.Services.AddDbContext<Contexto>(options =>
     options.UseOracle(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Adicionando CORS
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+// Ordem correta de uso do CORS
 app.UseRouting();
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 app.MapControllers(); // Configura os controllers
 
 app.Run();
--- DotNetApiMaxima/obj/Debug/net8.0/DotNetApiMaxima.AssemblyInfo.cs
+++ DotNetApiMaxima/obj/Debug/net8.0/DotNetApiMaxima.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetApiMaxima")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0d5b6893ae2665e81eae8ff50da6e8dea1ce232")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa25bc5228ae1d45dc02f10eedc622f66cd5037d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetApiMaxima")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetApiMaxima")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
