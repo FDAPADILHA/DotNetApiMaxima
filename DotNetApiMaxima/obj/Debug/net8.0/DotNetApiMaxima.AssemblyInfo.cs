97379d81bfa9d218da6a72fce7d2c31cb7b18f4e
Filipe do Amaral Padilha <filipe.padilha@maxima.local>
2025-01-20 10:47:30 -0300

    Ajustando a posição da nav bar e cores do header

--- DotNetApiMaxima/obj/Debug/net8.0/DotNetApiMaxima.AssemblyInfo.cs
+++ DotNetApiMaxima/obj/Debug/net8.0/DotNetApiMaxima.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetApiMaxima")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6f42703c41e9c3aacdc22c85199af6b76c302d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61147d254f7d081a14f17695cb0ae160409c3e83")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetApiMaxima")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetApiMaxima")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
